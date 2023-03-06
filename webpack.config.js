a1863975e24629f90bbfa4f944d28960a738b1d0
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-06 19:41:24 +0200

    Add main dependencies

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather app",
+    }),
+  ],
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }]
+            ],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+    ],
+  },
+};
