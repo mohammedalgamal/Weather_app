fee8d96153bb862ff1d8432fceb3fc64cd1fa8ff
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-07 19:12:36 +0200

    Add error handling

--- src/Scripts/data.js
+++ src/Scripts/data.js
@@ -1,8 +1,16 @@
 export default async function getData(city = "cairo", unit = "standard") {
     const APIKey = "d3038b3303b62168dd448fbeb4531d41";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`);
-    const data = await response.json();
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`);
+    let data;
+    if (response.status === 200) {
+        data = await response.json();
+    }
+    else {
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=cairo&APPID=${APIKey}`);
+        data = await response.json();
+    };
     const fullResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,alerts&units=${unit}&APPID=${APIKey}`);
     const fullData = await fullResponse.json();
     console.log(fullData);
+
 };
