85f69c59c72de95c19737c02b0c81f9e3d9f1c04
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-03-10 08:58:58 +0200

    Add try catch block on fetching the data

--- src/Scripts/data.js
+++ src/Scripts/data.js
@@ -1,16 +1,19 @@
 export default async function getData(city = "cairo", unit = "standard") {
     const APIKey = "d3038b3303b62168dd448fbeb4531d41";
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`);
-    let data;
-    if (response.status === 200) {
-        data = await response.json();
-    }
-    else {
-        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=cairo&APPID=${APIKey}`);
-        data = await response.json();
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`);
+        let data;
+        if (response.status === 200) {
+            data = await response.json();
+        }
+        else {
+            response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=cairo&APPID=${APIKey}`);
+            data = await response.json();
+        };
+        const fullResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,alerts&units=${unit}&APPID=${APIKey}`);
+        const fullData = await fullResponse.json();
+        console.log(fullData);
+    } catch (err) {
+        console.log(err);
     };
-    const fullResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,alerts&units=${unit}&APPID=${APIKey}`);
-    const fullData = await fullResponse.json();
-    console.log(fullData);
-
 };
